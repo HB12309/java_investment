893493ef499d94ec8d94794bf29e1937aa095508
hicery <qiuhengbin@codemao.cn>
2021-06-16 20:28:36 +0800

    key

---
+++ src/main/java/top/study/investment/algorithm/tree/LevelOrderBottom.java
@@ -0,0 +1,25 @@
+package algorithm.tree;
+
+import java.util.List;
+
+/**
+ * @Author qhb
+ * @Date 2021/6/16 1:56 下午
+ * @Version 1.0
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/
+ */
+public class LevelOrderBottom {
+    /**
+     * 2021年06月16日13:57:48
+     *
+     * 倒置来的，底下的先出来，优先考虑 Deque<Integer> stack = new LinkedList<>()
+     *
+     * hah, {@link LevelOrder#levelOrder(TreeNode)} 这个数组 reverse 不就好了？但这个蛋疼。。没必要吧
+     */
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/top/study/investment/algorithm/tree/ZigzaLevelOrder.java
@@ -0,0 +1,24 @@
+package algorithm.tree;
+
+import java.util.List;
+
+/**
+ * @Author qhb
+ * @Date 2021/6/16 1:52 下午
+ * @Version 1.0
+ * https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/
+ */
+public class ZigzaLevelOrder {
+    /**
+     * 2021年06月16日13:55:06
+     * 参考 {@link LevelOrder#levelOrder(TreeNode) } 层序遍历，并且判断奇数偶数，如果奇数就从左往右，偶数从右往左
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> zigzaLevelOrder(TreeNode root) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
